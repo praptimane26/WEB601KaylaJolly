b1e45cf8f0b33d784cb7bf87ea978aa4afcf09b1
praptimane26 <praptimane26@github.com>
2020-09-14 01:30:54 +1200

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
     return (
        <>
        <nav className="navbar">
@@ -12,7 +14,7 @@ function Navbar() {
                <Link to="/" className="navbar-logo">
                    Kayla Jolly Hairdresser
                    </Link> 
-                   <div className='menu-icon'>
+                   <div className='menu-icon' onClick={handleClick}>
                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                    </div>
            </div>
