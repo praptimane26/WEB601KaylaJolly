3b03dc8208abb777eb8f7f183cbe38a0381a7ea1
praptimane26 <praptimane26@github.com>
2020-09-14 01:22:00 +1200

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
    <>
+   <Router>
     <Navbar />
+    <Switch>
+      <Route path='/' exact />
+    </Switch>
+    </Router>
    </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,21 @@ import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
     return (
        <>
        <nav className="navbar">
            <div className="navbar-container">
                <Link to="/" className="navbar-logo">
                    Kayla Jolly Hairdresser
-                   </Link>    
-
+                   </Link> 
+                   <div className='menu-icon'>
+                       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                   </div>
            </div>
        </nav>
        </>
-    )
+    );
 }
 
 export default Navbar
