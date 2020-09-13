bd3d1e9b4db47853cda8a37e2ead93dd6beae026
praptimane26 <praptimane26@github.com>
2020-09-14 01:12:14 +1200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>yoo</h1>
-    </div>
+   <>
+    <Navbar />
+   </>
   );
 }
 
